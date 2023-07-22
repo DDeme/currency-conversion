55fba2aa3e9a41e95a6c545174f0519097b5f849
Dany <5726983+DDeme@users.noreply.github.com>
2023-07-22 13:14:52 +0200

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { Heading, Text, Box } from "@chakra-ui/react";
+
+export const Header = () => {
+  return (
+    <Box>
+      <Heading color={"gray.100"}>Purple Currency Conversion</Heading>
+      <Text color={"gray.100"}>
+        Best source for currency conversion and exchange rates
+      </Text>
+    </Box>
+  );
+};
