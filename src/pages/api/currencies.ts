2893b9d1a4a086d43f4dae32a6daa202d76a4993
Dany <5726983+DDeme@users.noreply.github.com>
2023-07-22 16:00:34 +0200

    extend currencies api  with symbols and details

--- src/pages/api/currencies.ts
+++ src/pages/api/currencies.ts
@@ -1,4 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
+import currenciesDetails from "./currencies.json";
+import { log } from "console";
+
+interface CurrencyDetail {
+  name: string;
+  symbol: string;
+  symbolNative: string;
+  decimalDigits: number;
+  rounding: number;
+  code: string;
+  namePlural: string;
+}
+interface CurrencyApiResponse {
+  [key: string]: CurrencyDetail;
+}
 
 export default async function handler(req: NextRequest, res: NextResponse) {
   if (req.method !== "GET") {
@@ -11,7 +26,15 @@ export default async function handler(req: NextRequest, res: NextResponse) {
       redirect: "follow",
     }
   );
-  const { currencies } = await response.json();
+
+  const { currencies: supportedCurrencies } = await response.json();
+  let currencies: CurrencyApiResponse = {};
+  for (const key in supportedCurrencies) {
+    const currencyDetail = currenciesDetails.find(({ code }) => code === key);
+    if (currencyDetail) {
+      currencies[key] = currencyDetail;
+    }
+  }
 
   return res.status(200).json({ currencies });
 }
