e16dd2e0d9f58bd6d7bd32dd4d95a723a809d4b3
Dany <5726983+DDeme@users.noreply.github.com>
2023-07-23 20:33:39 +0200

    exclude unneeded dependencies from functions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { withNx } = require("@nrwl/next/plugins/with-nx");
+const path = require("path");
+
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+    outputFileTracingExcludes: {
+      "*": [
+        // prettier-ignore
+        'node_modules/@swc/core-linux-x64-gnu',
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+        "node_modules/framer-motion/dist",
+      ],
+    },
+  },
+};
+
+module.exports = nextConfig;
