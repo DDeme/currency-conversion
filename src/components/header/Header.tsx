3d859d6b36f9d5fd5517f6053e78538bd230b44f
Dany <5726983+DDeme@users.noreply.github.com>
2023-07-27 23:10:03 +0200

    fix storybook

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import { Heading, Text, Box } from "@chakra-ui/react";
 export const Header = () => {
   return (
     <Box>
-      <Heading color={"gray.100"}>Purple Currency Conversion</Heading>
+      <Heading color={"gray.100"}>Currency Conversion</Heading>
       <Text color={"gray.100"}>
         Best source for currency conversion and exchange rates
       </Text>
