6325cb84017694fd1fef6f0e99c05d69834ab279
Dany <5726983+DDeme@users.noreply.github.com>
2023-07-29 12:54:54 +0200

    add currency conversion

--- src/components/form/inputs/currency-input/CurrencyInput.tsx
+++ src/components/form/inputs/currency-input/CurrencyInput.tsx
@@ -1,40 +1,42 @@
-import {
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-} from "@chakra-ui/react";
-
+import MaskedInput from "react-text-mask";
+import createNumberMask from "text-mask-addons/dist/createNumberMask";
 import React from "react";
+import { Input } from "@chakra-ui/react";
 
 interface Props {
   value: number;
   onChange: (value: number) => void;
 }
 
-export const CurrencyInput = ({ value, onChange }: Props) => {
-  const NumberFormatter = new Intl.NumberFormat("en-US");
-  const format = (val: number) => NumberFormatter.format(val);
-  const parse = (val: string) => {
-    const parsed = parseFloat(val.replace(/^\$/, ""));
-    return isNaN(parsed) ? 0 : parsed;
-  };
+const defaultMaskOptions = {
+  prefix: "",
+  suffix: "",
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ",",
+  allowDecimal: true,
+  decimalSymbol: ".",
+  decimalLimit: 2, // how many digits allowed after the decimal
+  integerLimit: 7, // limit length of integer numbers
+  allowNegative: false,
+  allowLeadingZeroes: false,
+  inputMode: "numeric",
+};
 
+const deMask = (value: string): number =>
+  value ? parseFloat(value.replace(/[^0-9.]/g, "")) : 0;
+
+export const CurrencyInput = ({ value, onChange }: Props) => {
+  const currencyMask = createNumberMask({
+    ...defaultMaskOptions,
+  });
   return (
-    <NumberInput
-      onChange={(valueString) => onChange(parse(valueString))}
-      value={format(value)}
-      inputMode="decimal"
-      precision={2}
-      min={0}
-      w={"100%"}
-    >
-      <NumberInputField />
-      <NumberInputStepper>
-        <NumberIncrementStepper />
-        <NumberDecrementStepper />
-      </NumberInputStepper>
-    </NumberInput>
+    <MaskedInput
+      mask={currencyMask}
+      value={value}
+      onChange={(event) => {
+        onChange(deMask(event.target.value));
+      }}
+      render={(ref, props) => <Input ref={ref} {...props} />}
+    />
   );
 };
